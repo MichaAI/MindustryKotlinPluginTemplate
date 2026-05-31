ed875bb95caf73aafa28d72b168b2f25573b6c4a
MichaAI <121772662+MichaAI@users.noreply.github.com>
2026-05-31 10:14:07 +0300

    Update build.gradle.kts to v8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import java.net.URI
+
 buildscript {
     repositories {
         mavenCentral()
     }
 
     extra.apply {
-        set("mindustryVersion", "v146")
-        set("kotlinVersion", "2.0.20")
+        set("mindustryVersion", "v158.1")
+        set("kotlinVersion", "2.2.0")
     }
 
     dependencies {
@@ -14,21 +16,40 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.2.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-    maven { url = uri("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository") }
-    maven { url = uri("https://www.jitpack.io") }
+    ivy{
+        url = URI("https://github.com/")
+        patternLayout{ artifact("/[organisation]/[module]/releases/download/[revision]/dependencies.jar")}
+        metadataSources{ artifact() }
+    }
+
+    //If the version is set to 'latest', downloads the latest Mindustry *release* as a dependency
+    ivy{
+        url = URI("https://github.com/")
+        patternLayout{artifact("/[organisation]/[module]/releases/[revision]/download/dependencies.jar")}
+        metadataSources{ artifact() }
+    }
+
+    //For depending on the absolute newest commit for Mindustry
+    ivy{
+        url = URI("https://github.com/")
+        patternLayout{artifact("/[organisation]/[module]/releases/download/master/[revision].jar")}
+        metadataSources{ artifact() }
+    }
+}
+
+java{
+    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-    compileOnly("com.github.Anuken.Arc:arc-core:${project.extra["mindustryVersion"]}")
-    compileOnly("com.github.Anuken.Mindustry:core:${project.extra["mindustryVersion"]}")
-    compileOnly("com.github.anuken.mindustry:server:${project.extra["mindustryVersion"]}")
+    compileOnly("Anuken:Mindustry:${project.extra["mindustryVersion"]}")
 }
 
 tasks.shadowJar {
